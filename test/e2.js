e1afa49ec95de767d9e31b8334004bd5991dc752
Endre Galaczi <endre.galaczi@made.com>
2015-12-05 00:12:23 +0000

    test for 2

---
+++ test/e2.js
@@ -0,0 +1,21 @@
+let chai = require('chai'),
+  path = require('path');
+
+chai.should();
+
+import {
+    list, cons, head, tail, isEmpty, show
+} from "../lib/framework.js";
+
+import {
+    lastButOne
+} from "../lib/e2.js";
+
+describe('#1 Last element of list', () => {
+  describe('1,2,3,4', () => {
+    let xs = list(1,2,3,4)
+
+    it('3', () => last(xs).should.be(4))
+  });
+
+});
